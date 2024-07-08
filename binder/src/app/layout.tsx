bc112d6605aea509fd154d82d5499f2006f789d0
BlastyTheDev <rmzha2234@gmail.com>
2024-07-08 15:48:35 +1000

    start nextjs project

---
+++ binder/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"]});
+export const metadata: Metadata = {
+  title: "Binder",
+  description: "API documentation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} bg-page-background`}>{children}</body>
+    </html>
+  );
+}
