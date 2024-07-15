a5feecacd1e4cd045d49c5522dc351b279b2cf72
BlastyTheDev <rmzha2234@gmail.com>
2024-07-15 19:58:13 +1000

    documenting RemoteHost API

--- binder/src/app/navbarlinks.tsx
+++ binder/src/app/navbarlinks.tsx
@@ -1,4 +1,12 @@
 export const links = [
     { category: "", name: "Overview", href: "/", icon: "description" },
-    { category: "category", name: "Find folder by ID", href: "/find-folder-by-id", icon: "article", method: "GET" },
+    { category: "security", name: "Login", href: "/login", icon: "article", method: "POST" },
+    { category: "security", name: "Signup", href: "/signup", icon: "article", method: "POST" },
+    { category: "security", name: "Logout", href: "/logout", icon: "article", method: "POST" },
+
+    { category: "discord", name: "Verify Account", href: "/verify-discord", icon: "article", method: "GET" },
+    { category: "discord", name: "Find Member", href: "/discord-member", icon: "article", method: "GET" },
+    { category: "discord", name: "Validate Username", href: "/discord-username-valid", icon: "article", method: "GET" },
+
+    { category: "minecraft", name: "List Running Servers", href: "/running-minecraft-servers", icon: "article", method: "GET" },
 ];
