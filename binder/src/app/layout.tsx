02c5e100ecf460a4b4132b8eba9da1cab78f7f77
BlastyTheDev <rmzha2234@gmail.com>
2024-07-17 17:46:14 +1000

    remove export from poppins in layout

--- binder/src/app/layout.tsx
+++ binder/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/ui/navbar";
 
-export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"]});
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"]});
 export const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"]});
 export const metadata: Metadata = {
     title: "Binder",
